7a444c7a18239a513bdfcdd8adbfb7d1b956efe6
Felipe <felwal1987@gmail.com>
2025-06-20 13:27:29 -0600

    Added Form to add Paragraphs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,32 @@
+import { useState } from "react";
+import data from "./data";
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  return (
+    <section className="section-center">
+      <h4>Tired of boring lorem ipsum?</h4>
+      <form className="lorem-form">
+        <label htmlFor="amount">Paragraphs:</label>
+        <input
+          type="number"
+          min={1}
+          step={1}
+          max={8}
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => {
+            setCount(+e.target.value);
+          }}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+    </section>
+  );
 };
 export default App;
